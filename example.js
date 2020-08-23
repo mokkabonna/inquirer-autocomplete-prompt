8970f3a592653f97c0a62525fe6ff51bdb40c5b8
Martin Hansen <martin@martinhansen.no>
2020-08-23 14:55:20 +0200

    Update example to use separators

--- example.js
+++ example.js
@@ -78,11 +78,12 @@ function searchStates(answers, input) {
   return new Promise(function(resolve) {
     setTimeout(function() {
       var fuzzyResult = fuzzy.filter(input, states);
-      resolve(
-        fuzzyResult.map(function(el) {
-          return el.original;
-        })
-      );
+      const results = fuzzyResult.map(function(el) {
+        return el.original;
+      });
+
+      results.push(new inquirer.Separator());
+      resolve(results);
     }, _.random(30, 500));
   });
 }
